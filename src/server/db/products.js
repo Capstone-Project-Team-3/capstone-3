20897c040ee02248cd87751fce3a691e36c43cc4
NaethanM <Naethanmartinez28@gmail.com>
2023-08-22 09:13:09 -0700

    added product functions to display products

---
+++ src/server/db/products.js
@@ -0,0 +1,30 @@
+const db = require('./client')
+
+
+const createProduct = async ({title, description, price, seller, availability, quantity, category, image}) => {
+    try {
+        const { rows: [product] } = await db.query(`
+        INSERT INTO products(title, description, price, seller, availability, quantity, category, image)
+        VALUES($1, $2, $3, $4, $5, $6, $7, $8)
+        RETURNING * `, [title, description, price, seller, availability, quantity, category, image])
+        return product;
+    } catch (err) {
+        throw err;
+    }
+}
+
+
+
+
+
+
+
+
+module.exports = {
+    createProduct,
+    // createOrders,
+    // createProductOrders,
+    // createBillingInfo
+};
+
+
