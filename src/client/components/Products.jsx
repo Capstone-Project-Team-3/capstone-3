61f362d367a8b2b1861ff8afbd2651f01651e782
NaethanM <Naethanmartinez28@gmail.com>
2023-08-25 11:49:06 -0700

    added all products and single products

--- src/client/components/Products.jsx
+++ src/client/components/Products.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import {useState,useEffect} from 'react'
-import {Link, useNavigate} from 'react-router-dom'
+import {Link, Navigate, useNavigate} from 'react-router-dom'
+
+
 const URL = `http://localhost:3000/api/` 
 
 function Products() {
 const [products, setProducts] = useState([]);
+const navigate = useNavigate()
+
+
 useEffect(() => {
 async function fetchAllProducts(){
-    try{
+    try {
         const response= await fetch(`${URL}products`)
         const data = await response.json()
-        console.log(data)
-
-    }catch(err){
+        setProducts(data.products);
+    } catch(err) {
         console.log(err)
     }
-        
 }
 fetchAllProducts()
 },[])
@@ -23,14 +26,16 @@ fetchAllProducts()
 
 
 
-
-
   return (
     <div>
         {
-            products.map((p) => <div>
-                
-            </div>)
+        products.map((p) => <div key={p.id} onClick={() => navigate(`/${p.id}`)}> 
+        <h2>{p.title}</h2>
+        {/* <img src={p.image} /> */}
+        <h4>description: {p.description}</h4>
+        <h4>price: ${p.price}</h4> 
+        <h4>seller: {p.seller}</h4>
+        </div>)
         }
     </div>
   )
