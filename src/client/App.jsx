e371d12c7e78c94be7ccc0924444dbb13be1cf0f
Seishin LeBlanc <seishinleblanc@gmail.com>
2023-08-30 11:52:48 -0500

    Corrected Session Storage and UseState.

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -4,23 +4,33 @@ import { Routes, Route} from 'react-router-dom'
 import SingleProduct from './components/SingleProduct';
 import Navigation from './components/Navigation';
 import Profile from './components/Profile';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BillingInfo from './components/BillingInfo';
 import Register from './components/Register';
 
 function App() {
   const [token, setToken] = useState(null)
   const [user, setUser] = useState('')
+  
+  useEffect (() => {
+    const sessiontoken = sessionStorage.getItem('token')
+    const sessionuser = sessionStorage.getItem('userSS')
+    setToken(sessiontoken),
+    setUser(sessionuser)
+  }, [])
+  
+
+
 
   return (
     <div>
       <Navigation token={token} user={user}/>
       <Routes>
-        <Route path='/login' element={ <Login setToken={setToken} setUser={setUser} token={token}/> } />
+        <Route path='/login' element={ <Login token={token}/> } />
         <Route path='/' element={ <Products token={token}/> } />
         <Route path='/products/:id' element={ <SingleProduct token={token}/> } />
-        <Route path='/users/myprofile' element={<Profile token={token} /> } />
-        <Route path='/mybillinginfo' element={ <BillingInfo/> } />
+        <Route path='/users/myprofile' element={<Profile token={token} user={user} /> } />
+        <Route path='/mybillinginfo' element={ <BillingInfo token={token} user={user} /> } />
         <Route path='/register' element={ <Register token={token}  user={user} /> } />
       </Routes>
     </div>
