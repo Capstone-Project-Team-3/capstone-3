f7bd0e7512adab2efbf110417e29a24afef47d8b
NaethanM <Naethanmartinez28@gmail.com>
2023-08-31 13:06:37 -0700

    cart functionalities

--- src/client/components/Cart.jsx
+++ src/client/components/Cart.jsx
@@ -1,87 +1,72 @@
 import React from 'react'
 import {useState,useEffect} from 'react'
-import {Link, Navigate, useNavigate} from 'react-router-dom'
 
 
 const URL = `http://localhost:3000/api/` 
 
 function Cart({user}) {
     const [orderid, setOrderid] = useState('');
-    const [productids, setProductids] = useState('');
+    // const [quantity, setProductids] = useState('');
     const [orderProducts, setOrderProducts] = useState([])
-    const userparsed =  JSON.parse(user)
-    // console.log(userparsed)
-    const userid = userparsed.id
+    // const userparsed =  JSON.parse(user)
+    // const userid = userparsed.id
+    const userid = user.id
+    // console.log(userid)
     useEffect(() => {
     async function getOrderUserId() {
-            try {
-                const response = await fetch(`${URL}orders/${userid}`)
-                const data = await response.json();
-                setOrderid(data.id)
+        try {
+            const response = await fetch(`${URL}orders/${userid}`)
+            const data = await response.json();
+            // console.log(data.id)
+            setOrderid(data.id)
 
-
-                async function getProductOrderByOrderId() {
-                    try {
-                         const response2 = await fetch(`${URL}productorders/${data.id}`)
-                         const data2 = await response2.json();
-                         console.log(data2)
-                         console.log(data2[0].product_id)
-                         const cartproducts = []
-                        for (let i = 0; i < data2.length; i++) {
-                            const productsresponse = await fetch(`${URL}products/${data2[i].product_id}`)
-                            const data3 = await productsresponse.json()
-                            cartproducts.push(data3)
-                        }
-                         console.log(cartproducts)
-                         setOrderProducts(cartproducts)
-                        // data.products = [];
-                        //  console.log('test', data2)
-                        //  await Promise.all(data2.map(async (productids) => {
-                        //     const product = await getProductId(productids.product_id)
-                        //     console.log(product)
-                        //     product.name = productids.product.name
-                        //     data.products.push(product)
-                        //  }))
-                        //  console.log('second one', data)
-                        //  return data;
-                    }catch (err) {
-                        console.log(err)
+            async function getProductOrderByOrderId() {
+        try {
+            const response2 = await fetch(`${URL}productorders/${data.id}`)
+            const data2 = await response2.json();
+            // console.log(data2)
+            // console.log(data2[0].product_id)
+            const cartproducts = []
+                for (let i = 0; i < data2.length; i++) {
+                    const productsresponse = await fetch(`${URL}products/${data2[i].product_id}`)
+                    // const quantityresponse = await fetch(`${URL}products/${data2[i].quantity}`)
+                    const data3 = await productsresponse.json()
+                    // const data4 = await quantityresponse.json()
+                    cartproducts.push(data3)
+                    // console.log(data4)
+                    // cartproducts.push(data4)
                     }
+                    // console.log(cartproducts)
+                    setOrderProducts(cartproducts)
+                }catch (err) {
+                    console.log(err)
+                }
                 }
                 getProductOrderByOrderId();
-
-                
             } catch (err) {
                 console.log(err)
             }
         }
         getOrderUserId();
     }, []) 
+    return (
+    <div>
+        <h2>{user.name}'s Cart</h2>
+        {
+            orderProducts.map((o) => <div key={o.id}>
+                <h3>{o?.title}</h3>
+                <img src={o?.image} />
+                <h3>Price: ${o?.price}</h3>
+                <br />
+                <button>Quantity</button>
+                {/* <button onClick={handleDelete()}>Remove</button> */}
+                <br />
+            </div>)
+        }
+        <button>Checkout</button>
+        
+    </div>
+        );
 }
-export default Cart
 
-// useEffect(() => {
-//     async function getOrderUserId() {
-//             try {
-//                 const response = await fetch(`${URL}orders/${userid}`)
-//                 const data = await response.json();
-//                 console.log(data)
-//                 setOrderid(data.id)
-//             } catch (err) {
-//                 console.log(err)
-//             }
-//         }
-//         async function getProductOrderByOrderId() {
-//             try {
-//                 const response2 = await fetch(`${URL}productorders/${orderid}`)
-//                 const data2 = await response2.json();
-//                 console.log(data2)
-//                 // setProductids(data2.product_id)
-//             }catch (err) {
-//                 console.log(err)
-//             }
-//         }
-//         getOrderUserId();
-//         getProductOrderByOrderId();
-// }, [])
-//  }
+export default Cart
